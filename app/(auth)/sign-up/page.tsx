d2bdfcec1874645b3fd52b688265c142c43ed4fb
Simon Julius <sjulius@getcarbon.co>
2024-04-19 17:22:45 +0100

    tailwind setup and folders achitecture

---
+++ app/(auth)/sign-up/page.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const SignUp = () => {
+  return <div className="bg-red-900 text-white">Sign up</div>;
+};
+
+export default SignUp;
---
+++ app/(root)/(home)/page.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const page = () => {
+  return <div className="h1-bold text-red-500">Home</div>;
+};
+
+export default page;
